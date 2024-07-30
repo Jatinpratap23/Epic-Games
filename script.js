ba8bf2afaf1be3592bdc5f03a5db72b1206b9a59
Jatinpratap23 <vibupratap1@gmail.com>
2024-07-30 12:50:54 -1100

    header logo,main manu

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+let array=[1,2,57,69,50,80]
+for(let i=0; i < array.length; i++ ) {
+    console.log(array[5]);
+}
+
+const a=9
+const b=-3
+const sum=(a-b)
+console.log(sum)
+
+
+
+for(let i=3; i < 1000; i=i+3) {
+    console.log(i);
+}
+
+// 1=> Write a function that takes a string as input and returns the string reversed?
+//2=> Write a function that takes an array of numbers as input and returns the largest number.?
+
+const d=[10,29,30,45,56,,77,0]
+function arry (d[]){
+    for(let i=0; i< a.length;i++) ml
+}
