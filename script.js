775a9d960f2e588f16d0394150b51797fb64e744
Jatinpratap23 <vibupratap1@gmail.com>
2024-08-19 20:21:36 -1100

    worked on container phase2

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 
 
-let array=[1,2,57,69,50,80]
-for(let i=0; i < array.length; i++ ) {
-    console.log(array[5]);
-}
+// let array=[1,2,57,69,50,80]
+// for(let i=0; i < array.length; i++ ) {
+//     console.log(array[5]);
+// }
 
-const a=9
-const b=-3
-const sum=(a-b)
-console.log(sum)
+// const a=9
+// const b=-3
+// const sum=(a-b)
+// console.log(sum)
 
 
 
-for(let i=3; i < 1000; i=i+3) {
-    console.log(i);
-}
+// for(let i=3; i < 1000; i=i+3) {
+//     console.log(i);
+// }
 
-// 1=> Write a function that takes a string as input and returns the string reversed?
-//2=> Write a function that takes an array of numbers as input and returns the largest number.?
+// // 1=> Write a function that takes a string as input and returns the string reversed?
+// //2=> Write a function that takes an array of numbers as input and returns the largest number.?
 
-const d=[10,29,30,45,56,,77,0]
-function arry (d[]){
-    for(let i=0; i< a.length;i++) ml
-}
-/**
- * Checks if a string is a palindrome.
- *
- * @param {string} str - The string to check.
- * @return {boolean} Returns true if the string is a palindrome, false otherwise.
- */
-function isPalindrome(str) {
-    // Remove non-alphanumeric characters and convert the string to lowercase
-    var cleanedStr = str.toLowerCase().replace(/[\W_]/g, '');
+// const d=[10,29,30,45,56,,77,0]
+// function arry (d[]){
+//     for(let i=0; i< a.length;i++) ml
+// }
+// /**
+//  * Checks if a string is a palindrome.
+//  *
+//  * @param {string} str - The string to check.
+//  * @return {boolean} Returns true if the string is a palindrome, false otherwise.
+//  */
+// function isPalindrome(str) {
+//     // Remove non-alphanumeric characters and convert the string to lowercase
+//     var cleanedStr = str.toLowerCase().replace(/[\W_]/g, '');
     
-    // Get the length of the cleaned string
-    var len = cleaned56Str.length;
+//     // Get the length of the cleaned string
+//     var len = cleaned56Str.length;
     
-    // Iterate through the string, comparing each character with its mirrored counterpart
-    for (var i = 0; i < len / 2; i++) {
-        if (cleanedStr[i] !== cleanedStr[len - 1 - i]) {
-            // If a mismatch is found, return false
-            return false;
-        }
-    }
+//     // Iterate through the string, comparing each character with its mirrored counterpart
+//     for (var i = 0; i < len / 2; i++) {
+//         if (cleanedStr[i] !== cleanedStr[len - 1 - i]) {
+//             // If a mismatch is found, return false
+//             return false;
+//         }
+//     }
     
-    // If all characters match their mirrored counterparts, return true
-    return true;
-}
+//     // If all characters match their mirrored counterparts, return true
+
+//     return true;
